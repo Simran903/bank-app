b4d8eeca528640c4fe7a5e40e35758b9b3d04467
sincerelyyyash <yta0088@gmail.com>
2024-06-28 03:17:37 +0530

    Refresh token added in schema

---
+++ server/prisma/migrations/20240627203004_refresh_token_added/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "refreshToken" TEXT;
+
+-- RenameForeignKey
+ALTER TABLE "Transaction" RENAME CONSTRAINT "Transaction_receivedTransfers_fkey" TO "Transaction_sentTransfers_fkey";
+
+-- RenameForeignKey
+ALTER TABLE "Transaction" RENAME CONSTRAINT "Transaction_sentTransfers_fkey" TO "Transaction_receivedTransfers_fkey";
