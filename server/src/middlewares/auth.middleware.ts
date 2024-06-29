1f233515888eb77602e23e60098995172da37557
sincerelyyyash <yta0088@gmail.com>
2024-06-29 13:17:44 +0530

    Auth middleware updated

--- server/src/middlewares/auth.middleware.ts
+++ server/src/middlewares/auth.middleware.ts
@@ -1,53 +1,51 @@
 
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { ApiError } from '../utils/apiError';
 import { asyncHandler } from '../utils/asyncHandler';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, User } from '@prisma/client';
 
-//TODO: add user type
 declare module 'express' {
   interface Request {
-    user?: any;
+    user?: User;
   }
 }
 
 const prisma = new PrismaClient();
 
-export const verifyJWT = asyncHandler(async (req: Request, res: Response, next: () => void) => {
+export const verifyJWT = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   try {
     const token = req.cookies?.accessToken || req.headers["authorization"]?.replace("Bearer ", "");
 
     if (!token) {
-      throw new ApiError({
+      return next(new ApiError({
         statusCode: 401,
         message: "Unauthorized request"
-      });
+      }));
     }
 
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string) as { id: number };
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string) as { userId: number };
 
     const user = await prisma.user.findUnique({
       where: {
-        id: decodedToken?.id
+        id: decodedToken.userId
       }
     });
 
     if (!user) {
-      throw new ApiError({
+      return next(new ApiError({
         statusCode: 401,
         message: "Invalid access token"
-      });
+      }));
     }
 
     req.user = user;
     next();
-
   } catch (error) {
-    throw new ApiError({
+    next(new ApiError({
       statusCode: 401,
       message: "Invalid Access Token"
-    });
+    }));
   }
 });
 
