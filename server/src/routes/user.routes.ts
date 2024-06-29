f8f3dbedb5aa9107080f1838c26667fd876b0614
sincerelyyyash <yta0088@gmail.com>
2024-06-29 22:14:01 +0530

    update password route added

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { signIn, signOut, signUp } from "../controllers/user.controller";
+import { signIn, signOut, signUp, updatePassword } from "../controllers/user.controller";
 import { verifyJWT } from "../middlewares/auth.middleware";
 
 const router = Router();
@@ -7,5 +7,7 @@ const router = Router();
 router.route("/signup").post(signUp);
 router.route("/signin").post(signIn);
 router.route("/signout").post(verifyJWT, signOut);
+router.route("/update-password").post(verifyJWT, updatePassword)
+
 
 export default router;
