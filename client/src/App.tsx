d3a83637fc1fd7e5e644b86b1a0f2ff847243508
Simran <itssimran903@gmail.com>
2024-06-30 01:30:09 +0530

    Update: Enhance landing page UI and user experience

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,26 @@
+import React from 'react';
 import Navbar from "./pages/Navbar";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
+import AboutUs from "./pages/AboutUs";
+import RevealSection from './components/RevealSection';
 
-export default function App() {
+
+const App: React.FC = () => {
   return (
     <div className="">
       <Navbar />
-      <Home />
-      <Services />
+      <RevealSection>
+        <Home />
+      </RevealSection>
+      <RevealSection>
+        <AboutUs />
+      </RevealSection>
+      <RevealSection>
+        <Services />
+      </RevealSection>
     </div>
-  )
+  );
 }
 
+export default App;
---
+++ client/src/components/NavbarButton/NavbarButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavbarButton: React.FC = () => {
+  return (
+    <div>NavbarButton</div>
+  )
+} 
+
+export default NavbarButton
