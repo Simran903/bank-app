cb7bdbcd469ce369437ae5982a453523c320160a
Simran <itssimran903@gmail.com>
2024-09-30 22:22:19 +0530

    UI update

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,13 @@
+import Accordion from "@/components/FAQs";
+import { Features } from "@/components/Features";
 import HomePage from "@/components/HomePage";
 
 export default function Home() {
   return (
     <div className="">
       <HomePage />
+      <Features />
+      <Accordion />
     </div>
   );
 }
