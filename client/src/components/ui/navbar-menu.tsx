29bcb9985f81a89f86c1bcfd68c1e87668c9660a
Simran <itssimran903@gmail.com>
2024-10-02 01:06:01 +0530

    UI Update

--- client/src/components/ui/navbar-menu.tsx
+++ client/src/components/ui/navbar-menu.tsx
@@ -1,8 +1,6 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-import Link from "next/link";
-import Image from "next/image";
 
 const transition = {
   type: "spring",
@@ -74,47 +72,5 @@ export const Menu = ({
     >
       {children}
     </nav>
-  );
-};
-
-export const ProductItem = ({
-  title,
-  description,
-  href,
-  src,
-}: {
-  title: string;
-  description: string;
-  href: string;
-  src: string;
-}) => {
-  return (
-    <Link href={href} className="flex space-x-2">
-      <Image
-        src={src}
-        width={140}
-        height={70}
-        alt={title}
-        className="flex-shrink-0 rounded-md shadow-2xl"
-      />
-      <div>
-        <h4 className="text-xl font-bold mb-1 text-black">
-          {title}
-        </h4>
-        <p className="text-neutral-700 text-sm max-w-[10rem] dark:text-neutral-300">
-          {description}
-        </p>
-      </div>
-    </Link>
-  );
-};
-
-export const HoveredLink = ({ children, ...rest }: any) => {
-  return (
-    <Link
-      {...rest}
-      className="text-black hover:text-gray-700 font-semibold ">
-      {children}
-    </Link>
   );
 };
