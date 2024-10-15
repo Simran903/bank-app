02f40deee699949da62da4bc761865dce6f81308
Simran <itssimran903@gmail.com>
2024-10-16 00:47:01 +0530

    Dashboard components

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,10 +2,14 @@ import Accordion from "@/components/FAQs";
 import { Features } from "@/components/Features";
 import Footer from "@/components/Footer";
 import HomePage from "@/components/HomePage";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div className="">
+      <div className="relative w-full flex items-center justify-center">
+          <Navbar />
+        </div>
       <HomePage />
       <Features />
       <Accordion />
--- client/src/app/signin/components/SigninForm.tsx
+++ client/src/app/signin/components/SigninForm.tsx
@@ -31,15 +31,17 @@ const SigninForm = () => {
     console.log(formData);
 
     try {
-      const response = await axios({
-        method: "post",
-        url: baseUrl + "/user/signin",
-        data: formData,
-        headers: {
-          "Content-Type": "application/json",
-          "Content-Length": undefined, // Let Axios handle the Content-Length
-        },
-      });
+      const response = await axios.post(
+        baseUrl + "/user/signin",
+        JSON.stringify(formData),
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      console.log(formData);
 
       const token = response.data.token;
       localStorage.setItem("authToken", token);
@@ -85,7 +87,7 @@ const SigninForm = () => {
           <button
             className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
             type="submit"
-            onClick={handleSubmit}
+            onClick={() => handleSubmit}
           >
             Sign in &rarr;
             <BottomGradient />
