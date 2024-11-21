d965f04a34840d889f5aad2c0ba07d9832a4012b
Simran <itssimran903@gmail.com>
2024-11-21 23:24:59 +0530

    fixed footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -8,21 +8,6 @@ const Footer: React.FC = () => {
         
         <h1 className="text-lg md:text-xl font-bold text-center md:text-left">Transsacto</h1>
 
-        <nav className="flex flex-wrap justify-center md:justify-start space-x-4 md:space-x-6 text-sm md:text-base">
-          <Link href="#home" className="hover:underline">
-            Home
-          </Link>
-          <Link href="#features" className="hover:underline">
-            Features
-          </Link>
-          <Link href="#accordion" className="hover:underline">
-            FAQs
-          </Link>
-          <Link href="/signin" className="hover:underline">
-            Signin
-          </Link>
-        </nav>
-
         <p className="text-xs md:text-sm text-center">
           &copy; {new Date().getFullYear()} Transsacto. All rights reserved.
         </p>
