d687c80410d7a26491e650d72efea9379baa8d08
Simran <itssimran903@gmail.com>
2025-06-04 02:00:46 +0530

    added withCredentials: true to Axios client to support cross-origin authentication

--- client/src/constants/axiosClient.ts
+++ client/src/constants/axiosClient.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const axiosClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
